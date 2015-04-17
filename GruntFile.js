1fa81305c395ee2c1c1ea433a932548c797f911b
Diliara Nasirova <contact@diliaranasirova.com>
2015-04-17 15:03:47 -0700

    Setup Sass with Grunt

---
+++ GruntFile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * Grunt Module
+ */
+module.exports = function (grunt) {
+
+    /**
+     * Configuration
+     */
+    grunt.initConfig({
+
+        /**
+         * Get package meta data
+         */
+        pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * Sass
+         */
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            },
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
+        },
+
+        /**
+         * Watch
+         */
+        watch: {
+            sass: {
+                files: '/scss/{,*/}*.{scss,sass}',
+                tasks: ['sass:dev']
+            }
+        }
+
+
+    });
+
+    /**
+     * Load Grunt plugins
+     */
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+
+    /**
+     * Default task
+     * Run `grunt` on the command line
+     */
+    grunt.registerTask('default', [
+        'sass:dev',
+        'watch'
+    ]);
+
+
+};
+
+
