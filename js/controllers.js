1e25dbd35954dda140fb97aa625ea6b09510cf8a
Diliara Nasirova <contact@diliaranasirova.com>
2015-04-24 11:57:31 -0700

    setup basic logics in angular to find correct/wrong answers

--- js/controllers.js
+++ js/controllers.js
@@ -5,6 +5,22 @@ gameControllers.controller('Game1PracticeController', function GameController($s
     $http.get('js/game1.json').success(function (text) {
         $scope.words = text;
     });
+
+
+
+
+    //Looking for
+    $scope.lookingFor = function (lookingForThis, theWordIs, wordIndex) {
+
+        wordID = "word_" + wordIndex;
+        if (lookingForThis == theWordIs) {
+             $('#' + wordID).addClass('correct');
+
+        } else {
+            $('#' + wordID).addClass('wrong');
+        }
+    }
+
 });
 
 
