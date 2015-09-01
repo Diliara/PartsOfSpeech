29b00c0e8979c8cd2ee4d22d77cb46b19f693a93
Diliara Nasirova <contact@diliaranasirova.com>
2015-09-01 16:16:48 -0700

    game 2 setup

--- js/routes.js
+++ js/routes.js
@@ -1,8 +1,17 @@
 angular.module('gameApp')
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/game1', {
-            templateUrl: 'partials/game1/index.html'
+        $routeProvider.when('/', {
+            templateUrl: 'partials/index.html'
         })
+            .when('/game1', {
+                templateUrl: 'partials/game1/index.html'
+            })
+
+            .when('/game2', {
+                templateUrl: 'partials/game2/index.html'
+            })
+
+
             .when('/game1/practice', {
                 templateUrl: 'partials/game1/practice/index.html',
                 controller: 'Game1PracticeController'
@@ -11,10 +20,21 @@ angular.module('gameApp')
                 templateUrl: 'partials/game1/challenge/index.html',
                 controller: 'Game1ChallengeController'
             })
-            .when('/home', {
-                redirectTo: '/game1'
+
+            .when('/game2/practice', {
+                templateUrl: 'partials/game2/practice/index.html',
+                controller: 'Game2PracticeController'
             })
+            .when('/game2/challenge', {
+                templateUrl: 'partials/game2/challenge/index.html',
+                controller: 'Game2ChallengeController'
+            })
+
+            .when('/home', {
+                redirectTo: '/'
+            })
+
             .otherwise({
-                redirectTo: '/game1'
+                redirectTo: '/'
             });
     }]);
