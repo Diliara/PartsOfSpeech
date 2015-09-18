8a1b312c33cdb958feafe75fb2e07971108013fd
Diliara Nasirova <contact@diliaranasirova.com>
2015-09-18 14:57:37 -0700

    parametric title of the game

--- js/controllers/game-controller.js
+++ js/controllers/game-controller.js
@@ -1,13 +1,37 @@
-
-
 angular.module('gameApp')
 
-    .controller('GameController', function (  $scope, $routeParams) {
+    .controller('GameController', function ($scope, $routeParams, $location) {
         $scope.sectionId = $routeParams.sectionId;
         globalSectionId = $scope.sectionId.substr(1);
         console.log("globalSectionId: " + globalSectionId);
 
         possibleSections = ['1-3', '1-4', '1-5', '1-6', 'test'];
+
+        $scope.getListOfSections = function () {
+
+
+            switch (globalSectionId) {
+                case '1-3':
+                    return 'Determiners, Articles, Pronouns'
+                    break;
+                case '1-4':
+                    return 'Determiners, Articles, Pronouns, Prepositions'
+                    break;
+                case '1-5':
+                    return 'Determiners, Articles, Pronouns, Prepositions, Verbs'
+                    break;
+                case '1-6':
+                    return 'Determiners, Articles, Pronouns, Prepositions, Verbs, Conjunctions'
+                    break;
+                default:
+                    $location.path("/home");
+
+            }
+
+
+        }
+
+
 
     });
 
