71dea33a0206fab9ec221e46423cff03f05e70de
Diliara Nasirova <contact@diliaranasirova.com>
2015-09-18 12:03:44 -0700

    refactoring files & code

--- js/controllers/text-words-controller.js
+++ js/controllers/text-words-controller.js
@@ -2,15 +2,11 @@
 var randomNumber = Math.floor((Math.random() * 3) + 1);
 
 angular.module('gameApp')
+
     .controller('TextWordsController', function ($scope, $http) {
-
-
-        console.log("random number: " + randomNumber);
-
+        console.log("loaded text #" + randomNumber);
 
         $http.get('json/game/text_option_' + randomNumber + '.json').success(function (text) {
             $scope.words = text;
         });
-
-
     });
